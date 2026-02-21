5f19aaaee461a649beb911e945d1be64441548b3
Mohiuddin <md.mohiuddin@strativ.se>
2026-02-21 15:43:56 +0600

    feat: worked

--- router_rebooter/reboot_router.py
+++ router_rebooter/reboot_router.py
@@ -1,20 +1,12 @@
 import os
-from unittest.mock import MagicMock, patch
 
-import pytest
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 load_dotenv()
-
-
-@pytest.fixture
-def mock_driver():
-    with patch("router_rebooter.reboot_router.webdriver.Chrome") as mock_chrome:
-        driver = MagicMock()
-        mock_chrome.return_value = driver
-        yield driver
 
 
 def get_username():
@@ -37,20 +29,39 @@ def get_status_url():
     return os.getenv("STATUS_URL")
 
 
-def navigate_to_login_page():
-    driver = webdriver.Chrome()
-    driver.get(get_base_url())
-    return driver
+def wait_until(driver, byId, selector, timeout=30):
+    WebDriverWait(driver, timeout).until(
+        EC.visibility_of_any_elements_located((byId, selector))
+    )[0]
 
 
 def login():
-    driver = navigate_to_login_page()
+    driver = webdriver.Chrome()
+    driver.get(get_base_url())
+
+    wait_until(driver, By.ID, "login_input_username")
     driver.find_element(By.ID, "login_input_username").send_keys(get_username())
+
+    wait_until(driver,By.ID, "login_input_password")
     driver.find_element(By.ID, "login_input_password").send_keys(get_password())
+
+    wait_until(driver, By.ID, "login_button")
     driver.find_element(By.ID, "login_button").click()
+
+    driver.get(get_reboot_url())
+
+    wait_until(driver, By.ID, "reboot_restart_button")
+    driver.find_element(By.ID, "reboot_restart_button").click()
+
+    confirm_xpath = "//button//span[contains(text(), 'Confirm')]/parent::button"
+    wait_until(
+        driver,
+        By.XPATH,
+        confirm_xpath
+    )
+    driver.find_element(By.XPATH, confirm_xpath).click()
 
     return driver
 
-
-def reboot_router(driver):
-    pass
+if __name__ == "__main__":
+    driver = login()
