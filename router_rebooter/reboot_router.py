6efd2d6cfca7e76150a8b5639dfc534853ce095e
Mohiuddin <md.mohiuddin@strativ.se>
2026-02-21 15:53:35 +0600

    refactor: slight refactor

--- router_rebooter/reboot_router.py
+++ router_rebooter/reboot_router.py
@@ -42,26 +42,28 @@ def login():
     wait_until(driver, By.ID, "login_input_username")
     driver.find_element(By.ID, "login_input_username").send_keys(get_username())
 
-    wait_until(driver,By.ID, "login_input_password")
+    wait_until(driver, By.ID, "login_input_password")
     driver.find_element(By.ID, "login_input_password").send_keys(get_password())
 
     wait_until(driver, By.ID, "login_button")
     driver.find_element(By.ID, "login_button").click()
 
+    return driver
+
+
+def reboot_router(driver):
     driver.get(get_reboot_url())
 
     wait_until(driver, By.ID, "reboot_restart_button")
     driver.find_element(By.ID, "reboot_restart_button").click()
 
     confirm_xpath = "//button//span[contains(text(), 'Confirm')]/parent::button"
-    wait_until(
-        driver,
-        By.XPATH,
-        confirm_xpath
-    )
+    wait_until(driver, By.XPATH, confirm_xpath)
     driver.find_element(By.XPATH, confirm_xpath).click()
 
     return driver
 
+
 if __name__ == "__main__":
     driver = login()
+    reboot_router(driver)
